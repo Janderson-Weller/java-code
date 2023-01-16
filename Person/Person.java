d06f66e1b240887b40eef009a5a64d1c87783cfe
Janderson-Weller <janderson.weller@gmail.com>
2023-01-15 21:15:04 -0400

    added country in Person

--- Person/Person.java
+++ Person/Person.java
@@ -4,11 +4,13 @@ public class Person {
     private String name;
     private int age;
     private String CPF;
+    private String country;
 
-    public Person(String name, int age, String cpf) {
+    public Person(String name, int age, String cpf, String country) {
         this.name = name;
         this.age = age;
         this.CPF =  cpf;
+        this.country = country;
     }
 
     public void setName(String name) {
@@ -35,9 +37,17 @@ public class Person {
         return this.CPF;
     }
 
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
     public static void main(String[] Args) {
-        Person p1 =  new Person("Janderson Weller", 21, "023.456.456-23");
+        Person p1 =  new Person("Janderson Weller", 21, "023.456.456-23", "Brazil");
         System.out.println("Olá " + p1.getName() + "," + " seja bem vindo!");
-        System.out.println("Seu CPF é: " + p1.getCPF() + " Sua idade é " + p1.getAge());
+        System.out.println("Seu CPF é: " + p1.getCPF() + " Sua idade é " + p1.getAge() + " País de oringem: " + p1.getCountry());
     }
 }
